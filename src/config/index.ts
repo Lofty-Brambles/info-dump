d332dbbfa5c38adf4b81a261da1323216ba761b4
Lofty | Brambles <prantikdas2004@gmail.com>
2022-11-11 13:44:42 +0530

    ♻ refactor | fixes config

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,24 +3,27 @@ import { config } from "dotenv";
 config();
 
 export const websiteURLs = {
-	dev:
-		process.env.DEV_WEBSITE_URL?.trim().replace(/\/$/g, "") ||
-		"http://localhost:3000",
-	prod:
-		(process.env.PROD_WEBSITE_URL as string).trim().replace(/\/$/g, "") ||
-		process.env.PUBLIC_VERCEL_URL!,
+	dev: (process.env.DEV_WEBSITE_URL || "http://localhost:3000")
+		.trim()
+		.replace(/\/$/g, ""),
+	prod: (process.env.PROD_WEBSITE_URL || process.env.PUBLIC_VERCEL_URL!)
+		.trim()
+		.replace(/\/$/g, ""),
 	current() {
 		return import.meta.env.DEV ? this.dev : this.prod;
 	},
 };
 
 export const blogDetails = {
-	name: process.env.BLOG_NAME?.trim() || "Info Dump",
-	description:
-		process.env.BLOG_DESCRIPTION?.trim() ||
-		"Info Dump is a simple blog aimed at getting some simple thoughts out to the world.",
-	author: process.env.BLOG_AUTHORS?.split(",").map(e => e.trim()) || [
-		"Lofty Brambles",
-	],
-	defaultTitle: process.env.BLOG_DEFAULT_TITLE || "Info Dump | Pen-time!",
+	name: (process.env.BLOG_NAME || "Info Dump").trim(),
+	description: (
+		process.env.BLOG_DESCRIPTION ||
+		"Info Dump is a simple blog aimed at getting some simple thoughts out to the world."
+	).trim(),
+	author: (process.env.BLOG_AUTHORS || "Lofty Brambles")
+		.split(",")
+		.map(e => e.trim()),
+	defaultTitle: (
+		process.env.BLOG_DEFAULT_TITLE || "Info Dump | Pen-time!"
+	).trim(),
 };
